e1497a34c39df59e5de1ea5836847226be000c6c
magnatavr <benoyevskiy@mail.ru>
2023-03-13 14:52:34 +0300

    done some tasks

--- mobilApp/src/features/actions/dangerosAction.ts
+++ mobilApp/src/features/actions/dangerosAction.ts
@@ -1,10 +1,14 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from 'axios'
 
+type MyPostData = {
+    arg1: number;
+    arg2: number;
+  }
 
 export const getDangerosThunk = createAsyncThunk(
 'dangeros/get',
-async (id) => axios.get(`/api/locations/{id}`)
+async (data:MyPostData) => axios.post(`/api/locations/{id}`,data)
 .then((res) => res.data)
 .catch((err)=> {
     throw new Error('location get Error');
--- mobilApp/src/features/actions/locationAction.ts
+++ mobilApp/src/features/actions/locationAction.ts
@@ -4,7 +4,7 @@ import axios from 'axios'
 
 export const getLocationsThunk = createAsyncThunk(
 'location/get',
-async (id) => axios.get(`/api/locations/{id}`)
+async (id) => axios.get(`/api/locations/${id}`)
 .then((res) => res.data)
 .catch((err)=> {
     throw new Error('location get Error');
