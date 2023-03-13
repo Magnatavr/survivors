c128685cc65eac48ad27f3953bf1313bb79b15dd
medvedds <nodejs977@gmail.com>
2023-03-13 15:15:34 +0300

    zzz

--- mobilApp/src/components/StartPage/StartPage.tsx
+++ mobilApp/src/components/StartPage/StartPage.tsx
@@ -1,14 +1,48 @@
-import React from 'react'
-import { View, Text, Button } from 'react-native';
+import React from 'react';
+import { View, Text, Button, StyleSheet, ImageBackground } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
+
+import backgroundImage from '../Images/surv.png';
 
 export default function StartPage() {
   const navigation = useNavigation();
 
   return (
-    <View>
-      <Text style={{ fontSize: 24, fontWeight: 'bold' }}>SURVIVOR</Text>
-      <Button title="Enter" onPress={() => navigation.navigate('Country')} />
-    </View>
-  )
+    <ImageBackground source={backgroundImage} style={styles.background}>
+      <View style={styles.titleContainer}>
+        <Text style={styles.title}>SURVIVORS</Text>
+      </View>
+      <View style={styles.buttonContainer}>
+        <Button title="Enter" onPress={() => navigation.navigate('Country')} color="#228B22" />
+      </View>
+    </ImageBackground>
+  );
 }
+
+const styles = StyleSheet.create({
+  background: {
+    flex: 1,
+    resizeMode: 'cover',
+    justifyContent: 'center',
+  },
+  titleContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingTop: 100,
+  },
+  buttonContainer: {
+    marginBottom: 50,
+    marginHorizontal: 50,
+    color: 'white'
+  },
+  title: {
+    fontSize: 36,
+    fontWeight: 'bold',
+    color: '#F5DEB3',
+  },
+});
+
+
+
+
